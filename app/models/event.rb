ece532e0e32dfab024b04aacec7661f02f4273a6
jason <wjcodes@example.com>
2020-08-13 18:06:13 -0400

    first commit

---
+++ app/models/event.rb
@@ -0,0 +1,26 @@
+class Event < ApplicationRecord
+
+  has_many :registrations, dependent: :destroy
+
+  validates :name, :location, presence: true
+
+  validates :description, length: { minimum: 25 }
+
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  validates :capacity, numericality: { only_integer: true, greater_than: 0 }
+
+  validates :image_file_name, format: { with: /\w+\.(jpg|png)\z/i, message: "must be a JPG or PNG image"}
+
+  def self.upcoming
+    where("starts_at > ?", Time.now).order("starts_at")
+  end
+
+  def free?
+    price.blank? || price.zero?
+  end
+
+  def sold_out?
+    (capacity - registrations.size).zero?
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :users
+  root "events#index"
+  
+  resources :events do
+    resources :registrations
+  end
+
+end
